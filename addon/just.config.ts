2e2cc41ad7574151df67cb8c7aac020f8de7560e
meerkat <40662117+MeerkatMC@users.noreply.github.com>
2026-02-01 16:36:42 +0000

    v1.1.1 candidate

--- addon/just.config.ts
+++ addon/just.config.ts
@@ -18,23 +18,22 @@ import {
 import path from "path";
 setupEnvironment(path.resolve(__dirname, ".env"));
 const projectName = getOrThrowFromProcess("PROJECT_NAME");
-const projectVersion = getOrThrowFromProcess("PROJECT_VERSION");
 const bundleTaskOptions: BundleTaskParameters = {
   entryPoint: path.join(__dirname, "./scripts/main.ts"),
   external: ["@minecraft/server", "@minecraft/server-ui"],
-  outfile: path.resolve(__dirname, "./bp/scripts/main.js"),
+  outfile: path.resolve(__dirname, "./dist/scripts/main.js"),
   minifyWhitespace: false,
   sourcemap: true,
   outputSourcemapPath: path.resolve(__dirname, "./dist/debug"),
 };
 const copyTaskOptions: CopyTaskParameters = {
-  copyToBehaviorPacks: [`./bp`],
-  copyToScripts: ["./bp/scripts"],
-  copyToResourcePacks: [`./rp`],
+  copyToBehaviorPacks: [`./behavior_packs/${projectName}`],
+  copyToScripts: ["./dist/scripts"],
+  copyToResourcePacks: [`./resource_packs/${projectName}`],
 };
 const mcaddonTaskOptions: ZipTaskParameters = {
   ...copyTaskOptions,
-  outputFile: `./dist/packages/${projectName}_v${projectVersion}.mcaddon`,
+  outputFile: `./dist/packages/${projectName}.mcaddon`,
 };
 task("lint", coreLint(["scripts/**/*.ts"], argv().fix));
 task("typescript", tscTask());
@@ -45,6 +44,12 @@ task("clean-collateral", cleanCollateralTask(STANDARD_CLEAN_PATHS));
 task("clean", parallel("clean-local", "clean-collateral"));
 task("copyArtifacts", copyTask(copyTaskOptions));
 task("package", series("clean-collateral", "copyArtifacts"));
-task("local-deploy", watchTask( ["scripts/*.ts", "bp/**/*.{json,lang,tga,ogg,png}", "rp/**/*.{json,lang,tga,ogg,png}"], series("clean-local", "build", "package") ) );
+task(
+  "local-deploy",
+  watchTask(
+    ["scripts/**/*.ts", "behavior_packs/**/*.{json,lang,tga,ogg,png}", "resource_packs/**/*.{json,lang,tga,ogg,png}"],
+    series("clean-local", "build", "package")
+  )
+);
 task("createMcaddonFile", mcaddonTask(mcaddonTaskOptions));
 task("mcaddon", series("clean-local", "build", "createMcaddonFile"));
